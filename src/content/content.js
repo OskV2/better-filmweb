f856d85b443a1d94aa72330a5ef4ceb266dda849
Maciej Buszkiewicz <maciej.buszkiewicz1@gmail.com>
2025-04-28 19:46:43 +0200

    feat: expanded project structure

--- src/content/content.js
+++ src/content/content.js
@@ -1,37 +1,45 @@
+import { getOverallWatchtime } from './user/user'
 
 const currentUrl = window.location.href
 console.log('Content script loaded for:', currentUrl)
 
+//  * Routing
 const HOME_PAGE = 'https://www.filmweb.pl/'
-const USER_PROFILE_PAGE = currentUrl.match("^https:\/\/www\.filmweb\.pl\/user\/[^\/]+$");  //  This regex will match 
-// const RECENTLY_RATED = document.querySelector("div.page__content")
+const USER_PROFILE_PAGE = currentUrl.match("^https:\/\/www\.filmweb\.pl\/user\/[^\/]+$");  //  Example:  
+const USER_VOTES_MOVIES_PAGE = currentUrl.match("^https:\\/\\/www\\.filmweb\\.pl\\/user\\/[^\\/]+#\\/votes\\/film$");
+const USER_VOTES_SERIES_PAGE = currentUrl.match("^https:\\/\\/www\\.filmweb\\.pl\\/user\\/[^\\/]+#\\/votes\\/serial$");
+const USER_WANT_TO_SEE_MOVIES_PAGE = currentUrl.match("^https:\\/\\/www\\.filmweb\\.pl\\/user\\/[^\\/]+#\\/wantToSee\\/film$");
+const USER_WANT_TO_SEE_SERIES_PAGE = currentUrl.match("^https:\\/\\/www\\.filmweb\\.pl\\/user\\/[^\\/]+#\\/wantToSee\\/serial$");
 
-// const h2 = RECENTLY_RATED.querySelector('h2');
 
-// console.log(USER_PROFILE_PAGE)
+//  * 
+const SESSION_COOKIE = document.cookie
+const CATEGORY_RATED = 'vote'
+const CATEGORY_WANT2SEE = 'want2see'
+const TYPE_MOVIE = 'film'
+const TYPE_SERIES = 'serial'
+
+window.navigation.addEventListener("navigate", (event) => {
+  console.log('location changed!');
+  console.log(event)
+})
 
 if (USER_PROFILE_PAGE) {
   const userName = USER_PROFILE_PAGE.toString().split('/')
-  console.log(userName)
+  console.log(`userName: ${userName}`)
   console.log(`You are currently at ${userName[4]} filmweb profile `)
+  getOverallWatchtime()
+  console.log('Does function work')
+  console.log(SESSION_COOKIE)
+
+  // Simple DOM manipulation to verify it's working
+  const div = document.createElement('div')
+  div.style.backgroundColor = 'red'
+  div.style.color = 'white'
+  div.style.padding = '10px'
+  div.style.zIndex = '9999'
+  div.textContent = 'Obliczanie watchTime\'u'
 }
-
-// Simple DOM manipulation to verify it's working
-const div = document.createElement('div')
-// div.style.position = 'fixed'
-// div.style.bottom = '20px'
-// div.style.right = '20px'
-div.style.backgroundColor = 'red'
-div.style.color = 'white'
-div.style.padding = '10px'
-div.style.zIndex = '9999'
-div.textContent = 'Obliczanie watchTime\'u'
-
-// document.body.appendChild(div)
-
-// If you want to use React, you'll need to import your ContentApp here
-// and mount it to a DOM element
-
 
 waitForElement('div.page__content h2')
   .then((elm) => {
@@ -42,21 +50,6 @@ waitForElement('div.page__content h2')
   .then(() => {
     calculateWatchTime()
   })
-
-// const displayWatchTime = async () => {
-//   await waitForElement('div.page__content h2')
-//   let watchTime = await calculateWatchTime();
-
-// }
-
-const calculateWatchTime = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("resolved");
-      return 10;
-    }, 2000);
-  });
-}
 
 function waitForElement(selector) {
   return new Promise(resolve => {
---
+++ src/content/user/user.js
@@ -0,0 +1,10 @@
+//  * /user/{user}
+
+export const getOverallWatchtime = () => {
+  console.log('This code runs only on /user/userName page')
+  console.log('This code runs also, when user refreshes the page')
+}
+
+export const getMoviesWatchtime = () => {}
+
+export const getSeriesWatchtime = () => {}
