3fbc6190c42a5a7f7baa8058a8a58cedc66a9746
Maciej Buszkiewicz <maciej.buszkiewicz1@gmail.com>
2025-04-29 23:46:30 +0200

    feat: moved logic from content.js to separate file
    also did huge code clean-up in content.js

--- src/content/content.js
+++ src/content/content.js
@@ -1,4 +1,5 @@
-import { getOverallWatchtime } from './user/user'
+import { userPageRenderer } from './user/user'
+
 
 const currentUrl = window.location.href
 console.log('Content script loaded for:', currentUrl)
@@ -12,61 +13,10 @@ const USER_WANT_TO_SEE_MOVIES_PAGE = currentUrl.match("^https:\\/\\/www\\.filmwe
 const USER_WANT_TO_SEE_SERIES_PAGE = currentUrl.match("^https:\\/\\/www\\.filmweb\\.pl\\/user\\/[^\\/]+#\\/wantToSee\\/serial$");
 
 
-//  * 
-const SESSION_COOKIE = document.cookie
-const CATEGORY_RATED = 'vote'
-const CATEGORY_WANT2SEE = 'want2see'
-const TYPE_MOVIE = 'film'
-const TYPE_SERIES = 'serial'
-
 window.navigation.addEventListener("navigate", (event) => {
   console.log('location changed!');
-  console.log(event)
+  // const userName = USER_PROFILE_PAGE.toString().split('/')
+  // console.log(`You are currently at ${userName[4]} filmweb profile `)
+
+  if (USER_PROFILE_PAGE) userPageRenderer()
 })
-
-if (USER_PROFILE_PAGE) {
-  const userName = USER_PROFILE_PAGE.toString().split('/')
-  console.log(`userName: ${userName}`)
-  console.log(`You are currently at ${userName[4]} filmweb profile `)
-  getOverallWatchtime()
-  console.log('Does function work')
-  console.log(SESSION_COOKIE)
-
-  // Simple DOM manipulation to verify it's working
-  const div = document.createElement('div')
-  div.style.backgroundColor = 'red'
-  div.style.color = 'white'
-  div.style.padding = '10px'
-  div.style.zIndex = '9999'
-  div.textContent = 'Obliczanie watchTime\'u'
-}
-
-waitForElement('div.page__content h2')
-  .then((elm) => {
-    console.log('Element is ready');
-    console.log(elm);
-    elm.appendChild(div);  // append the new element under the <a> tag
-  })
-  .then(() => {
-    calculateWatchTime()
-  })
-
-function waitForElement(selector) {
-  return new Promise(resolve => {
-      if (document.querySelector(selector)) {
-          return resolve(document.querySelector(selector));
-      }
-
-      const observer = new MutationObserver(mutations => {
-          if (document.querySelector(selector)) {
-              observer.disconnect();
-              resolve(document.querySelector(selector));
-          }
-      });
-
-      observer.observe(document.body, {
-          childList: true,
-          subtree: true
-      });
-  });
-}
--- src/content/user/user.js
+++ src/content/user/user.js
@@ -1,10 +1,28 @@
 //  * /user/{user}
 
-export const getOverallWatchtime = () => {
-  console.log('This code runs only on /user/userName page')
-  console.log('This code runs also, when user refreshes the page')
+import { waitForElement } from '../../../utils/wait-for-element'
+import {
+  watchtimeRatedOverall,
+  watchtimeRatedMovies,
+  watchtimeRatedSeries,
+} from '../../../utils/data';
+
+export const userPageRenderer = async () => {
+  // Simple DOM manipulation to verify it's working
+  const div = document.createElement('div')
+  div.style.backgroundColor = 'red'
+  div.style.color = 'white'
+  div.style.padding = '10px'
+  div.style.zIndex = '9999'
+  div.textContent = 'Obliczanie watchTime\'u'
+
+  waitForElement('div.page__content h2')
+  .then((elm) => {
+    console.log('Element is ready');
+    console.log(elm);
+    elm.appendChild(div);
+  })
+
+  const watchtime = await watchtimeRatedOverall()
+  div.textContent = `Watchtime: ${watchtime}`
 }
-
-export const getMoviesWatchtime = () => {}
-
-export const getSeriesWatchtime = () => {}
