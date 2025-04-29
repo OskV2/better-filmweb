ae6ad9e3b92c5bd3b19123bd296be59458a5b252
Maciej Buszkiewicz <maciej.buszkiewicz1@gmail.com>
2025-04-29 23:45:29 +0200

    feat: added a file in utils that combines all filmweb API actions

---
+++ utils/data.js
@@ -0,0 +1,95 @@
+import { getListOfIDs } from "../api/list-of-ids"
+import { getOverallWatchtime, getMoviesWatchtime, getSeriesWatchtime } from "../api/watchtime"
+
+//  * Constants
+const SESSION_COOKIE = document.cookie
+const CATEGORY_RATED = 'vote'
+const CATEGORY_WANT2SEE = 'want2see'
+const TYPE_MOVIE = 'film'
+const TYPE_SERIES = 'serial'
+
+//  Watchtime of all rated movies and series combined
+export const watchtimeRatedOverall = async () => {
+  try {
+    const movieIDs = await getListOfIDs(SESSION_COOKIE, CATEGORY_RATED, TYPE_MOVIE);
+    const seriesIDs = await getListOfIDs(SESSION_COOKIE, CATEGORY_RATED, TYPE_SERIES);
+    const watchtime = await getOverallWatchtime(movieIDs, seriesIDs)
+    console.log(watchtime)
+    return watchtime
+  } catch (error) {
+    console.error('Failed in routes (watchtime rated overall)');
+    throw new Error('Failed to get data!')
+  }
+}
+
+//  Watchtime of rated movies
+export const watchtimeRatedMovies = async () => {
+  try {
+    const IDs = await getListOfIDs(SESSION_COOKIE, CATEGORY_RATED, TYPE_MOVIE);
+    const watchtime = await getMoviesWatchtime(IDs)
+    return watchtime
+  } catch (error) {
+    console.error('Failed in routes');
+    throw new Error('Failed to get data!')
+  }
+}
+
+//  Watchtime of rated series
+export const watchtimeRatedSeries = async () => {
+  try {
+    const IDs = await getListOfIDs(SESSION_COOKIE, CATEGORY_RATED, TYPE_SERIES);
+    const watchtime = await getSeriesWatchtime(IDs)
+    return watchtime
+  } catch (error) {
+    console.error('Failed in routes');
+    throw new Error('Failed to get data!')
+  }
+}
+
+//  Watchtime of all movies and series added to "want2see" tab
+export const watchtimeWantToSeeOverall = async () => {
+  try {
+    const movieIDs = await getListOfIDs(SESSION_COOKIE, CATEGORY_WANT2SEE, TYPE_MOVIE);
+    const seriesIDs = await getListOfIDs(SESSION_COOKIE, CATEGORY_WANT2SEE, TYPE_SERIES);
+    const watchtime = await getOverallWatchtime(movieIDs, seriesIDs)
+    return watchtime
+  } catch (error) {
+    console.error('Failed in routes');
+    throw new Error('Failed to get data!')
+  }
+}
+
+//  Watchtime of all movies added to "want2see" tab
+export const watchtimeWantToSeeMovies = async () => {
+  try {
+    const IDs = await getListOfIDs(SESSION_COOKIE, CATEGORY_WANT2SEE, TYPE_MOVIE);
+    const watchtime = await getMoviesWatchtime(IDs)
+    return watchtime
+  } catch (error) {
+    console.error('Failed in routes');
+    throw new Error('Failed to get data!')
+  }
+}
+
+//  Watchtime of all series added to "want2see" tab
+export const watchtimeWantToSeeSeries = async () => {
+  try {
+    const IDs = await getListOfIDs(SESSION_COOKIE, CATEGORY_WANT2SEE, TYPE_SERIES);
+    const watchtime = await getSeriesWatchtime(IDs)
+    return watchtime
+  } catch (error) {
+    console.error('Failed in routes');
+    throw new Error('Failed to get data!')
+  }
+}
+
+//  Watchtime of series specified by the id parameter
+export const watchtimeSingleSeries = async (id) => {  
+  try {
+    const watchtime = await getSeriesWatchtime(id)
+    return watchtime
+  } catch (error) {
+    console.error('Failed in routes');
+    throw new Error('Failed to get data!')
+  }
+}
