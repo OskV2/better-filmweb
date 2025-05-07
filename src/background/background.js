f535cf1d1d97fbbd436e3558b73de151c1441396
Maciej Buszkiewicz <maciej.buszkiewicz1@gmail.com>
2025-05-07 23:28:11 +0200

    feat: minor changes in config

--- src/background/background.js
+++ src/background/background.js
@@ -1,10 +1,15 @@
 // Example background script
 chrome.runtime.onInstalled.addListener(() => {
-  console.log('Extension installed')
-})
+  console.log('Extension installed');
+});
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete') {
-    console.log('Tab updated:', tab.url)
+    console.log('Tab updated:', tab.url);
+
+    chrome.tabs.sendMessage(tabId, {
+      type: 'urlChanged',
+      url: tab.url,
+    })
   }
-})
+});
