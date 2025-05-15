98562cab34330c3da159f6db3dd20286ff6f9270
Maciej Buszkiewicz <maciej.buszkiewicz1@gmail.com>
2025-05-15 20:58:48 +0200

    feat: added another injected block on user profile page

--- src/content/user/user.js
+++ src/content/user/user.js
@@ -5,39 +5,64 @@ import {
   watchtimeRatedOverall,
   watchtimeRatedMovies,
   watchtimeRatedSeries,
+  watchtimeWantToSeeOverall,
+  watchtimeWantToSeeMovies,
+  watchtimeWantToSeeSeries
 } from '../../../utils/data';
 import { watchtimeParser } from '../../../utils/watchtime-parser';
 
 export const userPageRenderer = async () => {
 
   //  * If my element already exists return
-  //  * This if statement will avoid rendering my element multiple times
+  //  * This if statement will avoid rendering my element multiple times (this happened because filmweb is spaghetti coded)
   if (document.querySelector('.bf__title')) {
     return
   }
 
-  let htmlContent = `
+  const loadingContent = `
     <h2 class="bf__title">Better Filmweb</h2>
     <p class="bf__paragraph">Obliczanie watchtime'u</p>
   `
 
+  // * Render elements on webpage for rated positions
   const div = document.createElement('div')
-  div.innerHTML = htmlContent
+  div.innerHTML = loadingContent
   div.classList.add("bf__card");
 
-  const elm = await waitForElement('div.page__content h2')
+  const elm = await waitForElement('div.page__content h2')  // * This will select h2 with text "Ostatnio ocenione"
   elm.prepend(div);
 
-  const watchtimeOverall = await watchtimeRatedOverall()
-  const watchtimeMovies = await watchtimeRatedMovies()
-  const watchtimeSeries = await watchtimeRatedSeries()
+  const ratedMovies = await watchtimeRatedMovies()
+  const ratedSeries = await watchtimeRatedSeries()
+  const ratedOverall = await watchtimeRatedOverall()
 
-  htmlContent = `
-    <h2 class="bf__title">Better Filmweb</h2>
-    <p class="bf__paragraph">Watchtime filmów: <span class="bf__paragraph--yellow">${watchtimeParser(watchtimeMovies)}</span></p>
-    <p class="bf__paragraph">Watchtime seriali: <span class="bf__paragraph--yellow">${watchtimeParser(watchtimeSeries)}</span></p>
-    <p class="bf__paragraph">Watchtime sumaryczny: <span class="bf__paragraph--yellow">${watchtimeParser(watchtimeOverall)}</span></p>
+  const loadedRatedContent = `
+    <h2 class="bf__title">Better Filmweb - ocenione</h2>
+    <p class="bf__paragraph">Watchtime filmów: <span class="bf__paragraph--yellow">${watchtimeParser(ratedOverall)}</span></p>
+    <p class="bf__paragraph">Watchtime seriali: <span class="bf__paragraph--yellow">${watchtimeParser(ratedMovies)}</span></p>
+    <p class="bf__paragraph">Watchtime sumaryczny: <span class="bf__paragraph--yellow">${watchtimeParser(ratedSeries)}</span></p>
   `
-  div.innerHTML = htmlContent
+  div.innerHTML = loadedRatedContent
+
+  // * Render elements on webpage for positions added to want2see
+  const div2 = document.createElement('div')
+  div2.innerHTML = loadingContent
+  div2.classList.add("bf__card");
+
+  const elm2 = await waitForElement('#app > section:nth-child(4) > div:nth-child(1) > h2')  // * This will select h2 with text "Chcę zobaczyć"
+  console.log(elm2)
+  elm2.prepend(div2)
+
+  const w2sMovies = await watchtimeWantToSeeMovies()
+  const w2sSeries = await watchtimeWantToSeeSeries()
+  const w2sOverall = await watchtimeWantToSeeOverall()
+
+  const loadedW2sContent = `
+    <h2 class="bf__title">Better Filmweb - dodane do "Chcę zobaczyć"</h2>
+    <p class="bf__paragraph">Watchtime filmów: <span class="bf__paragraph--yellow">${watchtimeParser(w2sMovies)}</span></p>
+    <p class="bf__paragraph">Watchtime seriali: <span class="bf__paragraph--yellow">${watchtimeParser(w2sSeries)}</span></p>
+    <p class="bf__paragraph">Watchtime sumaryczny: <span class="bf__paragraph--yellow">${watchtimeParser(w2sOverall)}</span></p>
+  `
+  div2.innerHTML = loadedW2sContent
 }
 
